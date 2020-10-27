1d09d92b3c9eb0136cf3938adc23ec7a8d2a4e71
Eidson <ejas@cin.ufpe.br>
2020-10-27 20:16:46 -0300

    criando pontos e retas

--- bezier/sketch.js
+++ bezier/sketch.js
@@ -1,22 +1,34 @@
 var action;
 
 function chooseAction(act){
-  if (act == 'criarponto'){
-    action = 'ponto'
+
+  if (act == 'criarcurva'){
+    action = 'curva';
   }
 }
 
+var pts = [];
+
+
 function setup(){
   var canvas = createCanvas(700, 700);
-//  canvas.parent('sketch-div');
+ canvas.parent('sketch-div');
   background(220, 220, 220);
 }
 
 function draw(){
 
-  if (mouseIsPressed == true && mouseButton == LEFT && action == 'ponto'){
+
+  if (mouseIsPressed == true && mouseButton == LEFT && action == 'curva'){
     fill(0);
     ellipse(mouseX, mouseY, 10, 10);
-    action = ''
+    pts.push([mouseX, mouseY]);
+  }
+
+  if (keyIsPressed == true && keyCode == ENTER && action == 'curva'){
+    for (var i=0; i < pts.length-1; ++i) {
+      fill(0);
+      line(pts[i][0], pts[i][1], pts[i+1][0], pts[i+1][1]);
+    }
   }
 }
