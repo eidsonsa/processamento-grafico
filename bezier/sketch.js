532988e36f717895bf9f2a7fe8f22cba3c3d6e40
Eidson <ejas@cin.ufpe.br>
2020-10-27 20:53:46 -0300

    mostrando lista de retas

--- bezier/sketch.js
+++ bezier/sketch.js
@@ -1,13 +1,39 @@
 var action;
+var curvas = []
+var qtdCurvas = 0
+var atual;
 
 function chooseAction(act){
 
   if (act == 'criarcurva'){
     action = 'curva';
+    curvas[qtdCurvas] = [];
+    atual = qtdCurvas;
+    qtdCurvas++;
   }
 }
 
 var pts = [];
+
+function mouseClicked() {
+  if (action == 'curva'){
+    fill(0);
+    ellipse(mouseX, mouseY, 10, 10);
+    curvas[qtdCurvas - 1].push([mouseX, mouseY]);
+  }
+  return false;
+}
+
+function keyPressed(){
+  if (action == 'curva'){
+    for (var i=1; i < curvas[atual].length-1; ++i) {
+      fill(0);
+      line(curvas[atual][i][0], curvas[atual][i][1], curvas[atual][i+1][0], curvas[atual][i+1][1]);
+    }
+    document.getElementById('lista-curvas').innerHTML += '<li>' + 'Curva '+ (atual + 1) + ':' + (curvas[atual].length - 1) + '</li>';
+    action = '';
+  }
+}
 
 
 function setup(){
@@ -19,16 +45,6 @@ function setup(){
 function draw(){
 
 
-  if (mouseIsPressed == true && mouseButton == LEFT && action == 'curva'){
-    fill(0);
-    ellipse(mouseX, mouseY, 10, 10);
-    pts.push([mouseX, mouseY]);
-  }
 
-  if (keyIsPressed == true && keyCode == ENTER && action == 'curva'){
-    for (var i=0; i < pts.length-1; ++i) {
-      fill(0);
-      line(pts[i][0], pts[i][1], pts[i+1][0], pts[i+1][1]);
-    }
-  }
 }
+
