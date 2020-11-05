f0309db6c81a0ab43bce7f8b615799fa5733e91d
Acesarsilva <accs2@cin.ufpe.br>
2020-11-05 08:37:33 -0300

    Clean Code + Comentarios

--- bezier/sketch.js
+++ bezier/sketch.js
@@ -1,17 +1,17 @@
  
+//Valor Inicial de variaveis comuns
 var action;
 var curvas = []
 var qtdCurvas = 0
-var atual;
-//Implementar pegar o indice atual direto do html
 var index = 0
 var indiceaadicionar = -1;
-//var evaluationFactor = 0.001
 
+//Variaveis de Valor Inicial das Check Boxes
 var pontosOn = true;
 var curvasOn = true;
 var poligonosOn = true;
 
+//Funções de Interação com as Check Boxes
 function ativarPonto(){
   pontosOn = !pontosOn;
 }
@@ -24,11 +24,11 @@ function ativarPoligono(){
   poligonosOn = !poligonosOn;
 }
 
+//Controle de Ações
 function chooseAction(act){
-
   if (act == 'criarcurva'){
     evaluationFactor = 1 / document.getElementById('evaluationFactor').value
-    action = 'curva';
+    action = 'adicionar';
     curvas[qtdCurvas] = [];
     index = qtdCurvas;
     qtdCurvas++;
@@ -49,70 +49,40 @@ function chooseAction(act){
   }
 }
 
-var indicearemover = -1
-
+//Função de Interação com o Mouse 'Click'. Usamos para adicionar e remover pontos, a escolha de cada ação depende do do botão/action ativa.
 function canvasMouseClicked() {
-  if (action == 'curva'){
-    curvas[qtdCurvas - 1].push([mouseX, mouseY]);
-  }
   if (action == 'adicionar'){
     curvas[index].push([mouseX, mouseY]);
-    action = 'curva'
   }
   if (action == 'remover'){
     var ponto = findControlPoint(curvas[index], [mouseX, mouseY]);
     if (ponto != null){
-      curvas[index].splice(ponto, 1)
+      curvas[index].splice(ponto, 1);
     }
     if (curvas[index].length == 0){
-      curvas.splice(index, 1)
+      curvas.splice(index, 1);
+      if(index > 0){
+        --index;
+      }
     }
   }
   return false;
 }
 
+//Função de Interação com o Mouse 'Arrastar'. Usamos pra alterar os pontos de Controle da Reta Selecionada
 function mouseDragged(){
   if(curvas.length > 0 && curvas[index].length > 0){
     var indexControlPoint = findControlPoint(curvas[index],[mouseX,mouseY]);
     if(indexControlPoint != null){
       curvas[index][indexControlPoint][0] = mouseX;
       curvas[index][indexControlPoint][1] = mouseY;
+      action = 'grabPoint';
     }
   }
-  action = 'grabPoint';
   return false;
 } 
 
-function findControlPoint(curve, point){
-  for(cont = 0; cont < curve.length; ++cont){
-    if(Math.sqrt(Math.pow((curve[cont][0] - point[0]),2) + Math.pow((curve[cont][1] - point[1]),2) <= 1000)){  //1000 é o quanto o mouse pode se distanciar do ponto
-      return cont;
-    }
-  }
-  return null;
-}
-
-function adicionarPonto(indice){
-  action = 'adicionar';
-  indiceaadicionar = indice;
-}
-
-function removerPonto(indice){
-  indicearemover = indice
-  action = 'remover'
-}
-
-function keyPressed(){
-if (action == 'curva'){
-  document.getElementById('lista-curvas').innerHTML = '';
-  for (var i = 0; i < curvas.length; i++){
-    document.getElementById('lista-curvas').innerHTML += '<li>' + 'Curva '+ (i + 1) + ':' + (curvas[i].length) +
-    '<button onClick="adicionarPonto(' + i + ')">adicionar ponto</button>' + '<button onClick="removerPonto(' + i + ')"> remover ponto </button>' + '</li>';
-  }
-    action = '';
- }
-}
-
+//Mudança entre as Curvas feita pelas setas no HTML
 function mudarIndice(direcao){
   if (direcao == 'diminuir'){
     if (index != 0 ){
@@ -126,24 +96,66 @@ function mudarIndice(direcao){
   }
 }
 
-
+//Pegando o Fator de Avaliação do HTML
 window.onload=function(){var evaluationFactorHTML = document.getElementById('evaluationFactor');
-
 evaluationFactor.onchange = function(){
   evaluationFactor = 1 / evaluationFactorHTML.value 
-    
-}
-}
-
-
-function interpolation(startPoint, endPoint, factor){
-  return [ (1 - factor)*startPoint[0] + factor*endPoint[0] , (1 - factor)*startPoint[1] + factor*endPoint[1] ];
+    }
 }
 
+//Setup do Canvas
+function setup(){
+  var canvas = createCanvas(700, 700);
+  canvas.parent('sketch-div');
+  canvas.mouseClicked(canvasMouseClicked);
+  background(212, 219, 245);
+}
+
+function draw(){
+  background(212, 219, 245);
+  //Desenho dos Pontos de Controle
+  for(var i = 0; i < curvas.length; ++i){
+    if (pontosOn && i < curvas.length && curvas[i] != null){
+      for (var j = 0; j < curvas[i].length; j++){
+        fill(0, 41, 250)
+        stroke(0, 41, 250);
+        ellipse(curvas[i][j][0], curvas[i][j][1], 10, 10);
+      }
+    }
+    //Desenho das Curvas
+    if (curvasOn && i < curvas.length && curvas[i] != null && curvas[i].length > 1){
+      if(i == index){
+        fill('red');
+        stroke('red');  //A Curva Atual é Sempre a Vermelha.
+      }else{
+        fill(141, 7, 246);
+        stroke(141, 7, 246);
+      }
+      //Chamada do Algoritmo de DeCasteljau
+      var currentPoint = curvas[i][0]
+      var lastPoint = curvas[i][curvas[i].length-1];  //Ele não calcula o primeiro e o último ponto já que são triviais, foca só nos pontos internos.
+      for (var factor = evaluationFactor; factor <= 1; factor += evaluationFactor){
+        var nextPoint = deCasteljau(curvas[i],factor);
+        line(currentPoint[0], currentPoint[1], nextPoint[0], nextPoint[1]);
+        currentPoint = nextPoint;
+      }
+      line(currentPoint[0], currentPoint[1], lastPoint[0], lastPoint[1]);
+    }
+    //Desenho dos Poligonos de Controle
+    if (poligonosOn && i < curvas.length && curvas[i] != null && curvas[i].length > 1){
+      for (var j = 1; j < curvas[i].length; j++){
+        fill(255, 255, 5);
+        stroke(255, 255, 5);
+        line(curvas[i][j - 1][0], curvas[i][j - 1][1], curvas[i][j][0], curvas[i][j][1]);
+      }
+    }
+  }
+}
+
+//Algoritmo de DeCasteljau
 function deCasteljau (stageControlPoints, factor){
-  //console.log(stageControlPoints)
   if(stageControlPoints.length == 1){
-    return stageControlPoints[0];
+    return stageControlPoints[0]; 
   }else {
     var nextStageControlPoints = [];
     for(cont = 0; cont < stageControlPoints.length -1;++cont){
@@ -154,49 +166,17 @@ function deCasteljau (stageControlPoints, factor){
   }
 }
 
-
-
-function setup(){
-  var canvas = createCanvas(700, 700);
-  canvas.parent('sketch-div');
-  canvas.mouseClicked(canvasMouseClicked);
-  background(212, 219, 245);
+//Função de Interpolação
+function interpolation(startPoint, endPoint, factor){
+  return [ (1 - factor)*startPoint[0] + factor*endPoint[0] , (1 - factor)*startPoint[1] + factor*endPoint[1] ];
 }
 
-function draw(){
-  background(212, 219, 245);
-  for(var i = 0; i < curvas.length; ++i){
-    if (pontosOn && i < curvas.length){
-      for (var j = 0; j < curvas[i].length; j++){
-        fill(0, 41, 250)
-        stroke(0, 41, 250);
-        ellipse(curvas[i][j][0], curvas[i][j][1], 10, 10);
-      }
-    }
-    if (curvasOn && i < curvas.length && curvas[i].length > 1){
-      if(i == index){
-        fill('red');
-        stroke('red');
-      }else{
-        fill(141, 7, 246);
-        stroke(141, 7, 246);
-      }
-      var currentPoint = curvas[i][0]
-      var lastPoint = curvas[i][curvas[i].length-1];
-      for (var factor = evaluationFactor; factor <= 1; factor += evaluationFactor){
-        var nextPoint = deCasteljau(curvas[i],factor);
-        line(currentPoint[0], currentPoint[1], nextPoint[0], nextPoint[1]);
-        currentPoint = nextPoint;
-      }
-      line(currentPoint[0], currentPoint[1], lastPoint[0], lastPoint[1]);
-    }
-    if (poligonosOn && i < curvas.length && curvas[i].length > 1){
-      for (var j = 1; j < curvas[i].length; j++){
-        fill(255, 255, 5);
-        stroke(255, 255, 5);
-        line(curvas[i][j - 1][0], curvas[i][j - 1][1], curvas[i][j][0], curvas[i][j][1]);
-      }
+//Função Usada para Arrastar os Pontos de Controle da Reta Selecionada
+function findControlPoint(curve, point){
+  for(cont = 0; cont < curve.length; ++cont){
+    if(Math.sqrt(Math.pow((curve[cont][0] - point[0]),2) + Math.pow((curve[cont][1] - point[1]),2) <= 1000)){ 
+      return cont;                                               //1000 é o fator de distanciamento do mouse
     }
   }
+  return null;
 }
-
